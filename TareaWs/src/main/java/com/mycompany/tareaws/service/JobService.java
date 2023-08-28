e4f20e5e2dcd01fe2b8f50b9c920235b9db107ef
james <james@JAMESPORT>
2023-08-28 00:35:23 -0600

    Creacion del JobService y el metodo getJob

---
+++ TareaWs/src/main/java/com/mycompany/tareaws/service/JobService.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tareaws.service;
+
+import com.mycompany.tareaws.model.Job;
+import com.mycompany.tareaws.model.JobDto;
+import com.mycompany.tareaws.util.CodigoRespuesta;
+import com.mycompany.tareaws.util.Respuesta;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.NonUniqueResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author james
+ */
+public class JobService {
+    private static final Logger LOG = Logger.getLogger(JobService.class.getName());
+
+    @PersistenceContext(unitName = "TareaWsPU")
+    private EntityManager em;
+    public Respuesta getJob(Long jobId) {
+        try {
+            Query qryJob = em.createNamedQuery("Job.findByJobId", Job.class);
+            qryJob.setParameter("jobId", jobId);
+            return new Respuesta(true, CodigoRespuesta.CORRECTO, "", "", "Job", new JobDto((Job) qryJob.getSingleResult()));
+
+        } catch (NoResultException ex) {
+            return new Respuesta(false, CodigoRespuesta.ERROR_NOENCONTRADO, "No existe un puesto con el código ingresado.", "getJob NoResultException");
+        } catch (NonUniqueResultException ex) {
+            LOG.log(Level.SEVERE, "Ocurrio un error al consultar el puesto.", ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Ocurrio un error al consultar el puesto.", "getJob NonUniqueResultException");
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Ocurrio un error al consultar el puesto.", ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Ocurrio un error al consultar el puesto.", "getJob " + ex.getMessage());
+        }
+    }
+}
