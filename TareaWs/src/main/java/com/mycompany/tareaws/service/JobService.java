11bc0762e9e3b45324860a5170d8d3f3a97b9b85
james <james@JAMESPORT>
2023-08-28 00:43:01 -0600

    Metodo saveJob

--- TareaWs/src/main/java/com/mycompany/tareaws/service/JobService.java
+++ TareaWs/src/main/java/com/mycompany/tareaws/service/JobService.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.tareaws.service;
 
+import com.mycompany.tareaws.model.Employee;
+import com.mycompany.tareaws.model.EmployeeDto;
 import com.mycompany.tareaws.model.Job;
 import com.mycompany.tareaws.model.JobDto;
 import com.mycompany.tareaws.util.CodigoRespuesta;
@@ -39,6 +41,27 @@ public class JobService {
         } catch (Exception ex) {
             LOG.log(Level.SEVERE, "Ocurrio un error al consultar el puesto.", ex);
             return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Ocurrio un error al consultar el puesto.", "getJob " + ex.getMessage());
+        }
+    }
+    public Respuesta saveJob(JobDto jobDto) {
+        try {
+            Job job;
+            if (jobDto.getJobId() != null && jobDto.getJobId() > 0) {
+                job = em.find(Job.class, jobDto.getJobId());
+                if (job == null) {
+                    return new Respuesta(false, CodigoRespuesta.ERROR_NOENCONTRADO, "No se encrontró el puesto a modificar.", "saveJob NoResultException");
+                }
+                job.updateJob(jobDto);
+                job = em.merge(job);
+            } else {
+                job = new Job(jobDto);
+                em.persist(job);
+            }
+            em.flush();//si hay error lo marca aqui dentro
+            return new Respuesta(true, CodigoRespuesta.CORRECTO, "", "", "Job", new JobDto(job));
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Ocurrio un error al guardar el puesto.", ex);
+            return new Respuesta(false, CodigoRespuesta.ERROR_INTERNO, "Ocurrio un error al guardar el puesto.", "saveJob " + ex.getMessage());
         }
     }
 }
